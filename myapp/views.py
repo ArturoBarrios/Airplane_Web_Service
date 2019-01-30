769e56dcf9d653400e621777ad24166dac4256e1
Kefan <yang.3949@osu.edu>
2019-01-30 14:41:55 -0500

    deleting things

--- myapp/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-from rest_framework import viewsets
-from django.http import HttpResponse
-from django.views import View
-
-# Create your views here.
