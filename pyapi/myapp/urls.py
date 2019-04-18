62f01c9d04413b1f679178868459f2918ea8f07c
JarretLogan <jarretlogan20@gmail.com>
2019-04-18 19:48:12 -0400

    k

--- pyapi/myapp/urls.py
+++ pyapi/myapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('airplanes/<int:pk>', views.airplane_detail),
     path('airports/', views.airport_list),
     path('airports/<int:pk>', views.airport_detail),
-    path('customers/', views.airplane_list),
+    path('customer/', views.airplane_list),
     path('customers/<int:pk>', views.customer_detail),
     path('flights/', views.flight_list),
     path('flights/<int:pk>', views.flight_detail),
