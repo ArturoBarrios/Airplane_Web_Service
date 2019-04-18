d62930079de87f16bc86c8b81aea44cf6cd9fcb5
JarretLogan <jarretlogan20@gmail.com>
2019-04-17 20:29:12 -0400

    customer page change

--- pyapi/myapp/urls.py
+++ pyapi/myapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('airports/', views.airport_list),
     path('airports/<int:pk>', views.airport_detail),
     path('customers/', views.customer_list),
-    path('customers/<int:pk>', views.customer_detail),
+    path('customers/<int:pk>', views.airplane_detail),
     path('flights/', views.flight_list),
     path('flights/<int:pk>', views.flight_detail),
     path('customerairplane/', views.customer_list),
